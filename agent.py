4b476a4ad4288eb6de78ff66817adf10ff9c322e
Wenk, Joshua <joshua.d.wenk@gmail.com>
2023-11-27 13:19:00 -0500

    refactor agent.py

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 import mesa
 import enum
+
 import numpy as np
 
 ####################################################
@@ -19,8 +20,10 @@ class MyAgent(mesa.Agent):
         super().__init__(unique_id, model)   
         self.state = State.SUSCEPTIBLE  
         self.infection_time = 0
+        
         # Add number of days before a newly-infected person becomes infectious
-        self.exposed_time = np.random.normal(5, 2)  
+        self.latency_time = np.random.normal(5, 2)  
+        
         self.shots = 0  # Keep track of the number of vaccination shots
         
     def move(self):
@@ -36,14 +39,31 @@ class MyAgent(mesa.Agent):
         self.model.grid.move_agent(self, new_position)
 
 
-        
-    def status(self):
+    def update_status(self):
         """
+        This method simulates the contact between agents in a grid. 
         Checks the infection status of the agent and updates its state based on the following rules:
         - If the agent is infected and has been exposed for longer than their exposed time, they may either die or recover.
         - If the agent is vaccinated, they may become infected if they come into contact with an infected agent.
+        - If an agent is dead, it does not come into contact with other agents.
+        - If an agent is infected and comes into contact with a susceptible agent, the susceptible agent may become infected based on the transmission rate.
+
+        Note: Looks from the perspective of what may happen (or others may do) to me -- NOT what I may do to others!  (Transmission view orientation is Receive not Send)
         """
-        if self.state == State.INFECTED and self.model.schedule.time - self.infection_time > self.exposed_time:     
+
+        #Dead: stays dead! No zombies!
+        if self.state == State.DEAD:
+            return
+
+        #Recovered: stays Recovered (assume full immunity)
+        elif self.state == State.RECOVERED:
+            return
+
+        #only need to examine cellmates if get this far...
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])     
+
+        # Infected: can Die, stay Infected, or Recover
+        if self.state == State.INFECTED and self.model.schedule.time - self.infection_time > self.latency_time:     
             drate = self.model.death_rate
             alive = np.random.choice([0,1], p=[drate,1-drate])
             if alive == 0:
@@ -52,9 +72,19 @@ class MyAgent(mesa.Agent):
                 t = self.model.schedule.time-self.infection_time
                 if t >= self.recovery_time:          
                     self.state = State.RECOVERED
-        elif self.state == State.VACCINATED:
-            # If a vaccinated agent comes into contact with an infected agent, there's a chance they could become infected.
-            cellmates = self.model.grid.get_cell_list_contents([self.pos])
+
+        # Vaccinated: if comes into contact with an infected agent, there's still a (diminished) chance they could become infected. (assume partial immunity) Otherwise, remain Vax'd
+        elif self.state == State.VACCINATED:  
+            if len(cellmates) > 1:
+                for other in cellmates:
+                    if other.state is State.INFECTED and self.random.random() < self.model.ptrans * (0.5 if self.shots == 1 else 0.25 if self.shots >= 2 else 1):
+                        self.state = State.INFECTED
+                        self.infection_time = self.model.schedule.time
+                        self.recovery_time = self.model.get_recovery_time()
+
+        # Susceptible: Can contract through contact with Infected Agent in same cell; otherwise, remains Susceptible  
+        # [note: transition to VACCINATED happens in Model.step() not Agent.step()]
+        elif self.state == State.SUSCEPTIBLE:
             if len(cellmates) > 1:
                 for other in cellmates:
                     if other.state is State.INFECTED and self.random.random() < self.model.ptrans:
@@ -62,29 +92,12 @@ class MyAgent(mesa.Agent):
                         self.infection_time = self.model.schedule.time
                         self.recovery_time = self.model.get_recovery_time()
 
-    def contact(self):
-        """
-        This method simulates the contact between agents in a grid. If an agent is dead, it does not come into contact with other agents.
-        If an agent is infected and comes into contact with a susceptible agent, the susceptible agent may become infected based on the transmission rate.
-        """
-        if self.state == State.DEAD:
-            return
-        cellmates = self.model.grid.get_cell_list_contents([self.pos])       
-        if len(cellmates) > 1:
-            for other in cellmates:
-                # Agents with one shot get partial immunity (reduced transmission rate)
-                # while the transmission rates of agents with two shots are cut by 75%
-                if self.random.random() > self.model.ptrans * (0.5 if self.shots == 1 else 0.25 if self.shots >= 2 else 1):
-                    continue
-                if self.state is State.INFECTED and other.state is State.SUSCEPTIBLE:                    
-                    other.state = State.INFECTED
-                    other.infection_time = self.model.schedule.time
-                    other.recovery_time = self.model.get_recovery_time()
+        
+
 
     def step(self):
         """
-        Performs one step of the agent's life by calling the status, move, and contact methods in order.
+        Performs one step of the agent's life by calling status_update() then move() in order.
         """
-        self.status()
+        self.update_status()
         self.move()
-        self.contact()
